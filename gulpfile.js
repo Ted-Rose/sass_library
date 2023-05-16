64893fa550346fab8d9b479ac7490aeec85151c5
Ted-Rose <75977150+Ted-Rose@users.noreply.github.com>
2023-05-16 15:54:29 +0300

    Fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const purgecss = require('gulp-purgecss');
 const cssbeautify = require('gulp-cssbeautify');
 
 function buildStyles() {
-  return src('sass/**/*.scss')
+  return src('sass_library/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(purgecss({ content: ['*.html'] }))
     .pipe(cssbeautify()) // Add this line to format the CSS
@@ -12,7 +12,7 @@ function buildStyles() {
 }
 
 function watchTask() {
-  watch(['sass/**/*.scss', '*.html'], series(buildStyles));
+  watch(['sass_library/**/*.scss', '*.html'], series(buildStyles));
 }
 
 exports.default = watchTask;

4c3e4a2765b16459b73e1dfc65cc2d84bb6fa234
Tedis <tedis.rozenfelds@gmail.com>
2023-09-18 00:00:14 -0700

    Changed css files location

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const purgecss = require('gulp-purgecss');
-const cssbeautify = require('gulp-cssbeautify');
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
+const cssbeautify = require("gulp-cssbeautify");
 
 function buildStyles() {
-  return src('sass/**/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    // .pipe(purgecss({ content: ['*.html'] }))
-    .pipe(cssbeautify()) // Add this line to format the CSS
-    .pipe(dest('SubscriptionForm/src/routes/SubscribeToNewsletter'));
+    return (
+        src("sass/**/*.scss")
+            .pipe(sass({ outputStyle: "compressed" }))
+            // .pipe(purgecss({ content: ['*.html'] }))
+            .pipe(cssbeautify()) // Add this line to format the CSS
+            .pipe(dest("SubscriptionForm/src"))
+    );
 }
 
 function watchTask() {
-  watch(['sass/**/*.scss', '*.html'], series(buildStyles));
-  watch(['sass_library/**/*.scss', '*.html'], series(buildStyles));
+    watch(["sass/**/*.scss", "*.html"], series(buildStyles));
+    watch(["sass_library/**/*.scss", "*.html"], series(buildStyles));
 }
 
 exports.default = watchTask;

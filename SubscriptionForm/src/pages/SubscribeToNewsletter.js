87cf7f2cdc5cde4f42656665185a05845f5f8434
Tedis <tedis.rozenfelds@gmail.com>
2023-09-18 04:50:57 -0700

    Fixed missing top-bar

--- SubscriptionForm/src/pages/SubscribeToNewsletter.js
+++ SubscriptionForm/src/pages/SubscribeToNewsletter.js
@@ -4,43 +4,32 @@ import "./style.css";
 export const SubscribeToNewsletter = () => {
   return (
     <div className="mobile">
-      <div className="top-bar">
-        <div className="top-bar-2">
-          <img
-            className="logo-pineapple"
-            alt="Logo pineapple"
-            src="https://c.animaapp.com/YWiHxtUl/img/logo-pineapple@2x.png"
-          />
-          <div className="menu">
-            <div className="links">About</div>
-            <div className="links">How it works</div>
-            <div className="links">Contact</div>
+      <div className="overlap">
+        <div className="top-bar">
+          <div className="top-bar-2">
+            <img
+              className="logo-pineapple"
+              alt="Logo pineapple"
+              src="https://c.animaapp.com/YWiHxtUl/img/logo-pineapple@2x.png"
+            />
+            <div className="menu">
+              <div className="links">About</div>
+              <div className="links">How it works</div>
+              <div className="links">Contact</div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="overlap">
         <div className="content">
           <div className="text">
-            <p className="subheading">
-              Subscribe to our newsletter and get 10% discount on pineapple
-              glasses.
-            </p>
+            <p className="subheading">Subscribe to our newsletter and get 10% discount on pineapple glasses.</p>
             <div className="heading">Subscribe to newsletter</div>
           </div>
           <div className="input">
             <div className="overlap-group">
-              <img
-                className="line"
-                alt="Line"
-                src="https://c.animaapp.com/YWiHxtUl/img/line.svg"
-              />
+              <img className="line" alt="Line" src="https://c.animaapp.com/YWiHxtUl/img/line.svg" />
               <div className="div">
                 <div className="ic-arrow">
-                  <img
-                    className="img"
-                    alt="Ic arrow"
-                    src="https://c.animaapp.com/YWiHxtUl/img/ic-arrow.svg"
-                  />
+                  <img className="img" alt="Ic arrow" src="https://c.animaapp.com/YWiHxtUl/img/ic-arrow.svg" />
                 </div>
                 <p className="placeholder">Type your email address here…</p>
               </div>
@@ -60,25 +49,13 @@ export const SubscribeToNewsletter = () => {
             </div>
           </div>
           <div className="social">
-            <img
-              className="line-2"
-              alt="Line"
-              src="https://c.animaapp.com/YWiHxtUl/img/line-1.svg"
-            />
+            <img className="line-2" alt="Line" src="https://c.animaapp.com/YWiHxtUl/img/line-1.svg" />
             <div className="social-icons">
               <div className="youtube">
-                <img
-                  className="ic-youtube"
-                  alt="Ic youtube"
-                  src="https://c.animaapp.com/YWiHxtUl/img/ic-youtube.svg"
-                />
+                <img className="ic-youtube" alt="Ic youtube" src="https://c.animaapp.com/YWiHxtUl/img/ic-youtube.svg" />
               </div>
               <div className="twitter">
-                <img
-                  className="ic-twitter"
-                  alt="Ic twitter"
-                  src="https://c.animaapp.com/YWiHxtUl/img/ic-twitter.svg"
-                />
+                <img className="ic-twitter" alt="Ic twitter" src="https://c.animaapp.com/YWiHxtUl/img/ic-twitter.svg" />
               </div>
               <div className="instagram">
                 <img

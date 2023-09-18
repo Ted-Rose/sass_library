9a1d2b8042b554a71c00b01bdc1d6fb4d5422f1f
Tedis <tedis.rozenfelds@gmail.com>
2023-09-18 04:49:50 -0700

    Applied formatting

--- SubscriptionForm/src/pages/SubscribeToNewsletter.js
+++ SubscriptionForm/src/pages/SubscribeToNewsletter.js
@@ -4,18 +4,43 @@ import "./style.css";
 export const SubscribeToNewsletter = () => {
   return (
     <div className="mobile">
+      <div className="top-bar">
+        <div className="top-bar-2">
+          <img
+            className="logo-pineapple"
+            alt="Logo pineapple"
+            src="https://c.animaapp.com/YWiHxtUl/img/logo-pineapple@2x.png"
+          />
+          <div className="menu">
+            <div className="links">About</div>
+            <div className="links">How it works</div>
+            <div className="links">Contact</div>
+          </div>
+        </div>
+      </div>
       <div className="overlap">
         <div className="content">
           <div className="text">
-            <p className="subheading">Subscribe to our newsletter and get 10% discount on pineapple glasses.</p>
+            <p className="subheading">
+              Subscribe to our newsletter and get 10% discount on pineapple
+              glasses.
+            </p>
             <div className="heading">Subscribe to newsletter</div>
           </div>
           <div className="input">
             <div className="overlap-group">
-              <img className="line" alt="Line" src="https://c.animaapp.com/YWiHxtUl/img/line.svg" />
+              <img
+                className="line"
+                alt="Line"
+                src="https://c.animaapp.com/YWiHxtUl/img/line.svg"
+              />
               <div className="div">
                 <div className="ic-arrow">
-                  <img className="img" alt="Ic arrow" src="https://c.animaapp.com/YWiHxtUl/img/ic-arrow.svg" />
+                  <img
+                    className="img"
+                    alt="Ic arrow"
+                    src="https://c.animaapp.com/YWiHxtUl/img/ic-arrow.svg"
+                  />
                 </div>
                 <p className="placeholder">Type your email address here…</p>
               </div>
@@ -35,13 +60,25 @@ export const SubscribeToNewsletter = () => {
             </div>
           </div>
           <div className="social">
-            <img className="line-2" alt="Line" src="https://c.animaapp.com/YWiHxtUl/img/line-1.svg" />
+            <img
+              className="line-2"
+              alt="Line"
+              src="https://c.animaapp.com/YWiHxtUl/img/line-1.svg"
+            />
             <div className="social-icons">
               <div className="youtube">
-                <img className="ic-youtube" alt="Ic youtube" src="https://c.animaapp.com/YWiHxtUl/img/ic-youtube.svg" />
+                <img
+                  className="ic-youtube"
+                  alt="Ic youtube"
+                  src="https://c.animaapp.com/YWiHxtUl/img/ic-youtube.svg"
+                />
               </div>
               <div className="twitter">
-                <img className="ic-twitter" alt="Ic twitter" src="https://c.animaapp.com/YWiHxtUl/img/ic-twitter.svg" />
+                <img
+                  className="ic-twitter"
+                  alt="Ic twitter"
+                  src="https://c.animaapp.com/YWiHxtUl/img/ic-twitter.svg"
+                />
               </div>
               <div className="instagram">
                 <img
@@ -57,20 +94,6 @@ export const SubscribeToNewsletter = () => {
                   src="https://c.animaapp.com/YWiHxtUl/img/ic-facebook.svg"
                 />
               </div>
-            </div>
-          </div>
-        </div>
-        <div className="top-bar">
-          <div className="top-bar-2">
-            <img
-              className="logo-pineapple"
-              alt="Logo pineapple"
-              src="https://c.animaapp.com/YWiHxtUl/img/logo-pineapple@2x.png"
-            />
-            <div className="menu">
-              <div className="links">About</div>
-              <div className="links">How it works</div>
-              <div className="links">Contact</div>
             </div>
           </div>
         </div>

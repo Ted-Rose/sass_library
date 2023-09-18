fa6d8fbf0de716d6890b0d278cb20df2ad8bff60
Tedis <tedis.rozenfelds@gmail.com>
2023-09-18 07:15:36 -0700

    Terms, Icons PerfectPixel CSS

--- SubscriptionForm/src/components/Content.js
+++ SubscriptionForm/src/components/Content.js
@@ -11,56 +11,8 @@ const Content = () => (
       <div className="heading">Subscribe to newsletter</div>
     </div>
     <EmailForm />
-    <div className="TOS">
-      <p className="i-agree-to-terms-of">
-        <span className="text-wrapper">I agree to </span>
-        <span className="span">terms of service</span>
-      </p>
-      <div className="checkmark">
-        <img
-          className="ic-checkmark"
-          alt="Ic checkmark"
-          src="https://c.animaapp.com/YWiHxtUl/img/ic-checkmark.svg"
-        />
-      </div>
-    </div>
-    <div className="social">
-      <img
-        className="line-2"
-        alt="Line"
-        src="https://c.animaapp.com/YWiHxtUl/img/line-1.svg"
-      />
-      <div className="social-icons">
-        <div className="youtube">
-          <img
-            className="ic-youtube"
-            alt="Ic youtube"
-            src="https://c.animaapp.com/YWiHxtUl/img/ic-youtube.svg"
-          />
-        </div>
-        <div className="twitter">
-          <img
-            className="ic-twitter"
-            alt="Ic twitter"
-            src="https://c.animaapp.com/YWiHxtUl/img/ic-twitter.svg"
-          />
-        </div>
-        <div className="instagram">
-          <img
-            className="ic-instagram"
-            alt="Ic instagram"
-            src="https://c.animaapp.com/YWiHxtUl/img/ic-instagram.svg"
-          />
-        </div>
-        <div className="facebook">
-          <img
-            className="ic-facebook"
-            alt="Ic facebook"
-            src="https://c.animaapp.com/YWiHxtUl/img/ic-facebook.svg"
-          />
-        </div>
-      </div>
-    </div>
+    <Terms />
+    <Icons />
   </div>
 );
 

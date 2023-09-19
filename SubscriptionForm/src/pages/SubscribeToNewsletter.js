4ff471b3125234a5acc563d8ac41b34005c7c8fe
Tedis <tedis.rozenfelds@gmail.com>
2023-09-19 01:37:29 -0700

    Centered className content

--- SubscriptionForm/src/pages/SubscribeToNewsletter.js
+++ SubscriptionForm/src/pages/SubscribeToNewsletter.js
@@ -1,25 +1,18 @@
 import React from "react";
 import Content from "../components/Content";
+import Header from "../components/Header";
 
 export const SubscribeToNewsletter = () => {
   return (
     <div className="container-form">
       <div className="overlap">
-        <div className="top-bar">
-          <div className="top-bar-2">
-            <img
-              className="logo-pineapple"
-              alt="Logo pineapple"
-              src="https://c.animaapp.com/YWiHxtUl/img/logo-pineapple@2x.png"
-            />
-            <div className="menu">
-              <div className="links">About</div>
-              <div className="links">How it works</div>
-              <div className="links">Contact</div>
-            </div>
-          </div>
-        </div>
+        <Header />
         <Content />
+        <img
+          className="background-image"
+          alt="Background image"
+          src="https://c.animaapp.com/X2atuPdF/img/image.png"
+        />
       </div>
     </div>
   );

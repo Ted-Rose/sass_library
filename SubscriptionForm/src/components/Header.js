206a4748f0c6fa2fbbee26113df8df615e59f697
Tedis <tedis.rozenfelds@gmail.com>
2023-09-18 23:20:14 -0700

    Updated Header classNames

--- SubscriptionForm/src/components/Header.js
+++ SubscriptionForm/src/components/Header.js
@@ -2,24 +2,28 @@ import React from "react";
 import PineappleText from "./Images/pineapple-text.svg";
 import LogoPineapple from "./Images/logo_pineapple.svg";
 
-const Header = (/* { links } */) => {
+const Header = () => {
     return (
-        <div className="navbar-white navbar-md navbar-xl">
-            <div className="container pr-0">
-                <img src={LogoPineapple} alt="SVG pineapple logo" />
+        <div className="top-bar">
+            <div className="top-bar-2">
+                <img
+                    src={LogoPineapple}
+                    alt="SVG pineapple logo"
+                    className="logo-pineapple"
+                />
                 <img
                     src={PineappleText}
                     alt="SVG pineapple text"
                     className="sm-hide"
                 />
-                <ul class="display-f pl-5 pr-2 pt-2-5 pb-2-5 mt-n-4 mb-n-4">
-                    <li class="font-md mr-2 btn-gray-2">
+                <ul class="menu">
+                    <li class="links">
                         <a href="#">About</a>
                     </li>
-                    <li class="font-md mr-2 btn-gray-2">
+                    <li class="links">
                         <a href="#">How it works</a>
                     </li>
-                    <li class="font-md btn-gray-2">
+                    <li class="links">
                         <a href="#">Contact</a>
                     </li>
                 </ul>

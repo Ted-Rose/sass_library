b68e1aff35d32219c69eb9d699772dd3a9f854ce
Ted-Rose <75977150+Ted-Rose@users.noreply.github.com>
2023-09-19 22:37:20 -0700

    Social icons change on hover and active

--- SubscriptionForm/src/pages/SubscribeToNewsletter.js
+++ SubscriptionForm/src/pages/SubscribeToNewsletter.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Content from "../components/Content";
 import Header from "../components/Header";
-import image_summer from './../components/Images/image_summer.png';
 
 export const SubscribeToNewsletter = () => {
   return (
@@ -10,6 +9,8 @@ export const SubscribeToNewsletter = () => {
         <Header />
         <Content />
       </div>
+      <svg className="ic-facebook"> <use xlinkHref="./Images/ic_facebook.svg" /> </svg>
+
     </div>
   );
 };
